17c520c0f4bef028168e7f60a0637335fcf66148
Mark Noone <mark.noone@davranetworks.com>
2016-08-23 12:33:36 +0100

    Added Mosquitto client and Repo cleanup

---
+++ Ruban StatsD Files/ruban-monitor-console.js
@@ -0,0 +1,182 @@
+/*jshint node:true, laxcomma:true */
+/* CLI Test Command Structure:
+echo "metric_name:metric_value|type_specification" | nc -u -w0 127.0.0.1 8125 */
+
+var util = require('util');
+var http = require('http');
+var defs = require('./monitorMetrics.json');
+
+function RubanBackend(startupTime, config, emitter){
+
+  var isDevice = false;
+
+  var self = this;
+  this.lastFlush = startupTime;
+  this.lastException = startupTime;
+  this.config = config;
+
+  console.log("Backend Loaded!");
+  emitter.on('flush', function(timestamp, metrics) { self.flush(timestamp, metrics); });
+  emitter.on('status', function(callback) { self.status(callback); });
+  console.log("Checking If Monitor device Exists ...");
+  isAlreadyRubanDevice(config, 'Ruban Monitor', function(err, booleanResult){
+    if(booleanResult === true){
+      console.log('Device found Launching Monitor...');
+    } else {
+      console.log('Device not found. Creating Device ..');
+      createDevice(config, function(){
+        console.log('Device Created! \nLaunching Monitor!');
+      });
+    }
+  });
+}
+
+
+RubanBackend.prototype.flush = function(timestamp, metrics) {
+  console.log('Flushing Ruban stats at ', new Date(timestamp * 1000).toString());
+
+  var timers = metrics.timers;
+  var data = [];
+  var timestamp = (new Date).getTime();
+  for (var timer in timers) {
+    var values = timers[timer];
+    for (var index =0; index < values.length; index++) {
+      if (values[index] > 0) {
+        data.push({ UUID: this.config.appKey, timestamp: timestamp, name: "davra.response.times", value: values[index], msg_type: "datum"});
+	       console.log("Pushed Data!");
+      }
+    }
+  }
+
+  var gauges = metrics.gauges;
+  console.log(gauges);
+  for (var gauge in gauges) {
+    console.log(gauge);
+    var value = gauges[gauge];
+    if (value > 0) {
+      console.log("Bigger");
+      var tbp = { UUID: this.config.appKey, timestamp: timestamp, name: gauge, value: value, msg_type: "datum", }
+      console.log(tbp);
+      data.push(tbp);
+       console.log("Pushed Data!");
+    }
+  }
+
+  console.log("Sending!")
+  if (data.length > 0) {
+    sendDataToRuBAN(this.config, data);
+  }
+};
+
+//http.get.hello.response_time
+function parseKey(key) {
+  return key;
+}
+
+function postRequest(host, url, body) {
+  return new http.request({
+    hostname: host,
+    port: 58000,
+    path: url,
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(body)
+    }
+  });
+}
+
+function putRequest(host, url, body) {
+  return new http.request({
+    hostname: host,
+    port: 58000,
+    path: url,
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(body)
+    }
+  });
+}
+
+function isAlreadyRubanDevice(config, devicename, callback){
+	getRequest(config.rubanUrl, '/eem/api/v1/devices', function(err, body){
+		if(err) {
+			console.log(err);
+			process.exit(0);
+		}
+		var jsonRes = JSON.parse(body);
+    for(key in jsonRes.records){
+      for(item in jsonRes.records[key]){
+          if(item === 'name'){
+            var check = jsonRes.records[key][item];
+            if(check === devicename){
+              callback (null, true)
+              return;
+            }
+          }
+      }
+    }
+    callback(null, false);
+	});
+
+}
+
+function getRequest(host, url, callback) {
+    var req = new http.get({
+    hostname: host,
+    port: 58000,
+    path: url
+    }, function(res){
+	    //console.log('STATUS: ' + res.statusCode);
+  	    //console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+	    var bodyChunks = [];
+  	    res.on('data', function(chunk) {
+    	    	bodyChunks.push(chunk);
+  	    }).on('end', function() {
+    	    var body = Buffer.concat(bodyChunks);
+            	//console.log('BODY: ' + body);
+		      callback(null, body.toString());
+
+  	    })
+    });
+    req.on('error', function(e){
+	console.log("Error: " + e.message);
+	callback(err, null)
+	});
+
+}
+
+function createDevice(config, callback) {
+  var data = {
+    "serialNumber": config.appKey,
+    "name": "Ruban Monitor",
+    "ipAddress": config.rubanUrl
+  };
+  console.log("Data created! AppKey: " + config.appKey)
+  var body = JSON.stringify(data);
+  var request = postRequest(config.rubanUrl, "/eem/api/v1/devices", body);
+  request.end(body)
+  callback();
+}
+
+function sendDataToRuBAN(config, data) {
+  console.log("Sending Stats ..");
+  var body = JSON.stringify(data);
+  var request = putRequest(config.rubanUrl, "/api/v1/iotdata", body);
+  request.end(body, function(err, res){
+  	if(err) console.log(err);
+  });
+}
+
+RubanBackend.prototype.status = function(write) {
+  ['lastFlush', 'lastException'].forEach(function(key) {
+    write(null, 'console', key, this[key]);
+  }, this);
+};
+
+exports.init = function(startupTime, config, events) {
+  var instance = new RubanBackend(startupTime, config, events);
+  return true;
+};
---
+++ Ruban StatsD Files/rubanconfig.js
@@ -0,0 +1,7 @@
+{
+ port: 8125,
+ backends: [ "./backends/ruban-monitor-console" ],
+ rubanUrl: "192.168.10.229",
+ appKey: "RuBAN"
+ // authKey: "O8sdExtOiRzjgUUpCArexZ3Y1eLoX3ORXOZkukFQdKH8BysmOUj8J3HsywCEbpiJ9CJ3qlYJjjcQzF6onGM73343UhYyvfYncvu7y5cCSvrjzzXOumAl00Brd3mhEJ7MMxFwgVnBCnGnCodpQlBTTsVJuD4ESZA9fppxNlc51mjxpoTI9CnonWmZY11IZPkbt3xR2py5E5xtOp2XExx81QNBEKUhBdsvk4JCFyMGFKXl0Kq774LF77dqKPsrlkjz"
+}
